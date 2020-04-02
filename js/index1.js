38d3eba5ee6bfc8a91015fa8585a7d0cbfc8af42
Audrey BONNET <audrey.bonnet.45@outlook.com>
2020-04-02 08:58:52 +0200

    modif html page 1

--- js/index1.js
+++ js/index1.js
@@ -1,18 +1,18 @@
 
-const mEuro = parseFloat(prompt("Hello ! Write a positive number in € to convert in $ :"));
+// const mEuro = parseFloat(prompt("Hello ! Write a positive number in € to convert in $ :"));
 
-if ( mEuro >= 0) {
-    const mDollars = mEuro * 1.10 ;
-    window.alert("Your price in € convert is : " + mDollars.toFixed(2) + "$");
-} else {
-    window.alert("You need write a positive number too convert !");
-}
-
-
-// const euro = parseFloat(prompt("Entrez une valeur en euro"));
-// if (isNaN(euro) || euro < 0) {
-//     alert("Veuillez rentrer un nombre positif !");
+// if ( mEuro >= 0) {
+//     const mDollars = mEuro * 1.10 ;
+//     window.alert("Your price in € convert is : " + mDollars.toFixed(2) + "$");
 // } else {
-//     const dollars = euro * 1.1;
-//     alert("Votre conversion de " + euro.toFixed(1) + "€ correspond à " + dollars.toFixed(1) + "$");
+//     window.alert("You need write a positive number too convert !");
 // }
+
+
+const euro = parseFloat(prompt("Entrez une valeur en euro"));
+if (isNaN(euro) || euro < 0) {
+    alert("Veuillez rentrer un nombre positif !");
+} else {
+    const dollars = euro * 1.1;
+    alert("Votre conversion de " + euro.toFixed(1) + "€ correspond à " + dollars.toFixed(1) + "$");
+}
