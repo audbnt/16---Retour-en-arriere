6a9c3564832aa2dbbaa97ca91c8c5b09bee71c0c
Audrey BONNET <audrey.bonnet.45@outlook.com>
2020-04-02 14:20:25 +0200

    modif programme serial number

--- js/index4.js
+++ js/index4.js
@@ -63,55 +63,66 @@
 
 // correction Antho :
 
-const serialNumber = document.getElementById("numeroSerie");
+const serialNum = document.getElementById("serialNumber");
+const validation = document.getElementById("bpValid");
+const message = document.getElementById("messValErr");
 let isValid = true;
 
-// == Etape 1 : Vérification du format == //
-if (serialNumber.length !== 19) {
-    isValid = false;
-    console.error("Regle 1 : Mauvaise longueur !");
-} else if (serialNumber[4] !== '-' || serialNumber[9] !== '-' || serialNumber[14] !== '-') {
-    isValid = false;
-    console.error("Regle 1 : Mauvais format (tiret) !");
-} else if (isNaN(serialNumber.split('-').join(''))) {
-    isValid = false;
-    console.error("Regle 1 : Mauvais format (chiffres) !");
-} else if (parseInt(serialNumber.split('-').join('')) !== parseFloat(serialNumber.split('-').join(''))) {
-    isValid = false;
-    console.error("Regle 1 : Mauvais format (y'a un point) !");
+function funcInput(){
+
+    const serNum = serialNum.value ;
+
+    // == Etape 1 : Vérification du format == //
+    if (serNum.length !== 19) {
+        isValid = false;
+        message.innerHTML = "Regle 1 : Mauvaise longueur !";
+    } else if (serNum[4] !== '-' || serNum[9] !== '-' || serNum[14] !== '-') {
+        isValid = false;
+        message.innerHTML = "Regle 1 : Mauvais format (tiret) !";
+    } else if (isNaN(serNum.split('-').join(''))) {
+        isValid = false;
+        message.innerHTML = "Regle 1 : Mauvais format (chiffres) !";
+    } else if (parseInt(serNum.split('-').join('')) !== parseFloat(serNum.split('-').join(''))) {
+        isValid = false;
+        message.innerHTML = "Regle 1 : Mauvais format (y'a un point) !";
+    }
+
+    const groups = serNum.split('-');
+
+    // == Etape 2 : Groupe 1 & Groupe 3 == //
+    if (groups[0] !== groups[2].split('').reverse().join('')) {
+        isValid = false;
+        message.innerHTML = "Regle 2 !";
+    }
+
+    const group3 = parseInt(groups[2]);
+    const group2 = parseInt(groups[1]);
+
+    // == Etape 3 : Groupe 3 & Groupe 2 == //
+    if ((group3 * 7) % 10000 !== group2) {
+        isValid = false;
+        message.innerHTML = "Regle 3 !";
+    }
+
+    const group1 = parseInt(groups[0]);
+    const group4 = parseInt(groups[3]);
+
+    // == Etape 4 : La somme == //
+    if ((group1 + group2 + group3 + group4) % 10000 !== 0) {
+        isValid = false;
+        message.innerHTML = "Regle 4 !";
+    }
+
+    if (isValid) {
+        message.innerHTML = "Le numéro de série est valide !" ;
+    } else {
+        message.innerHTML = "Le numéro de série n'est pas valide !";
+    }
 }
 
-const groups = serialNumber.split('-');
+validation.addEventListener("click", funcInput);
 
-// == Etape 2 : Groupe 1 & Groupe 3 == //
-if (groups[0] !== groups[2].split('').reverse().join('')) {
-    isValid = false;
-    console.error("Regle 2 !");
-}
 
-const group3 = parseInt(groups[2]);
-const group2 = parseInt(groups[1]);
-
-// == Etape 3 : Groupe 3 & Groupe 2 == //
-if ((group3 * 7) % 10000 !== group2) {
-    isValid = false;
-    console.error("Regle 3 !");
-}
-
-const group1 = parseInt(groups[0]);
-const group4 = parseInt(groups[3]);
-
-// == Etape 4 : La somme == //
-if ((group1 + group2 + group3 + group4) % 10000 !== 0) {
-    isValid = false;
-    console.error("Regle 4 !");
-}
-
-if (isValid) {
-    document.write("Le numéro de série est valide !");
-} else {
-    document.write("Le numéro de série n'est pas valide !");
-}
 
 
 // 0x00-0000 -> représente une valeur hexa pour ça on peut préciser le type de parseInt -> parseInt("0x10", 10);
