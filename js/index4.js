12d51fe1eb1b13abafa2cb8818c5d15921af34b0
Audrey BONNET <audrey.bonnet.45@outlook.com>
2020-04-03 12:29:01 +0200

    modif exos serial number

--- js/index4.js
+++ js/index4.js
@@ -89,9 +89,55 @@ const serialInput = document.getElementById("serialInput");
 const checkButton = document.getElementById("checkButton");
 const messageViewer = document.getElementById("message");
 
+function checkSerial(serialNumber) {
+    const groups = serialNumber.split('-');
+
+    // == Etape 2 : Groupe 1 & Groupe 3 == //
+    if (groups[0] !== groups[2].split('').reverse().join('')) {
+        console.error("Regle 2 !");
+        return false;
+    }
+
+    const group3 = parseInt(groups[2]);
+    const group2 = parseInt(groups[1]);
+
+    // == Etape 3 : Groupe 3 & Groupe 2 == //
+    if ((group3 * 7) % 10000 !== group2) {
+        console.error("Regle 3 !");
+        return false;
+    }
+
+    const group1 = parseInt(groups[0]);
+    const group4 = parseInt(groups[3]);
+
+    // == Etape 4 : La somme == //
+    if ((group1 + group2 + group3 + group4) % 10000 !== 0) {
+        console.error("Regle 4 !");
+        return false;
+    }
+
+    return true;
+}
+
 serialInput.addEventListener("input", function() {
-    if (ev.data !== null && !ev.data.split("").every(c => "0123456789".indexOf(c) !== -1)) {
+    if (serialInput.value.split("").some(c => "0123456789".indexOf(c) === -1)) {
         serialInput.value = serialInput.value.split("").filter(c => "0123456789".indexOf(c) !== -1).join("");
+    }
+
+    // if (serialInput.value.length > 4) {
+    //     serialInput.value = serialInput.value.slice(0, 4) + '-' + serialInput.value.slice(4)
+    // }
+    // if (serialInput.value.length > 9) {
+    //     serialInput.value = serialInput.value.slice(0, 9) + '-' + serialInput.value.slice(9)
+    // }
+    // if (serialInput.value.length > 14) {
+    //     serialInput.value = serialInput.value.slice(0, 14) + '-' + serialInput.value.slice(14)
+    // }
+
+    for (let i = 4; i <= 14; i += 5) {
+        if (serialInput.value.length > i) {
+            serialInput.value = serialInput.value.slice(0, i) + '-' + serialInput.value.slice(i);
+        }
     }
     
     const serialNumber = serialInput.value;
@@ -112,3 +158,14 @@ serialInput.addEventListener("input", function() {
     }
 });
 
+checkButton.addEventListener("click", function(){
+
+    if(checkSerial(serialInput.value)){
+        messageViewer.textContent = "Votre de numéro de série est valide !";
+        messageViewer.className = "valid";
+    }else{
+        messageViewer.textContent = "Votre de numéro de série est invalide !";
+        messageViewer.className = "error";
+    }
+
+});
